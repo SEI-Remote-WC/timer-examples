87c413513e6f12676ee37d1646e15cfd655e339a
ManliestBen <benjamintmanley@gmail.com>
2021-06-24 11:37:00 -0500

    add solution code

--- bootstrap-timer/js/main.js
+++ bootstrap-timer/js/main.js
@@ -4,3 +4,53 @@ const startBtn = document.getElementById('startButton')
 const secBar = document.getElementById('seconds')
 const minBar = document.getElementById('minutes')
 const hrBar = document.getElementById('hours')
+
+let timerIntervalId
+let hr, min, sec, seconds = 0
+
+startBtn.addEventListener('click', ()=> {
+  startBtn.textContent = (startBtn.textContent === "Start") ? "Pause" : "Start"
+  if (timerIntervalId) {
+    clearInterval(timerIntervalId)
+    timerIntervalId = null
+  } else {
+    startTimer();
+  }
+})
+
+resetBtn.addEventListener('click', ()=> {
+  startBtn.textContent = "Start"
+  clearInterval(timerIntervalId)
+  seconds = 0
+  timerIntervalId = null
+  render()
+})
+
+function tick() {
+  seconds++
+  console.log(seconds)
+  render()
+}
+
+function startTimer(){
+  timerIntervalId = setInterval(tick, 1000)
+}
+
+function render() {
+  min = Math.floor(seconds / 60);
+  hr = Math.floor(seconds / 3600);
+  sec = seconds % 60;
+  min = min % 60;
+  hr = hr % 24;
+  if (sec < 10) {
+    timerEl.innerText = `${min}:0${sec}`
+  } else {
+    timerEl.innerText = `${min}:${sec}`
+  }
+  secBar.style = `width: ${sec/60*100}%`
+  secBar.innerText = sec;
+  minBar.style = `width: ${(min/60*100)}%`
+  minBar.innerText = min;
+  hrBar.style = `width: ${(hr/24*100)}%`
+  hrBar.innerText = hr;
+}
--- count-down-timer/main.js
+++ count-down-timer/main.js
@@ -1,1 +1,11 @@
 let countdownEl = document.getElementById('countdown')
+let timeLeft= 10;
+
+let timer = setInterval(function() {
+    countdownEl.textContent = timeLeft + ' seconds remaining.';
+    timeLeft -= 1;
+    if (timeLeft < 0) {
+        countdownEl.textContent = 'Finished!'
+    }
+
+}, 1000)
